21436fff75ad11de19d1bc6129c2c531230f67de
Adib Saikali <asaikali@gmail.com>
2022-04-03 00:39:33 -0400

    project skeleton

---
+++ webauthn-basics/src/main/java/com/example/WebauthnBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebauthnBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebauthnBasicsApplication.class, args);
+	}
+
+}
