fa38cd966148bf47d63e3647251a78682a2570cd
Adib Saikali <asaikali@gmail.com>
2022-04-06 00:40:38 -0400

    Polish

---
+++ authserver-basics/confidential-client/src/main/java/com/example/ConfidentialClientApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfidentialClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfidentialClientApplication.class, args);
+  }
+}
