12449725b58243fec03581c4df2a11503ec8a8b2
asaikali <asaikali@gmail.com>
2022-04-09 17:54:09 -0400

    Polish

--- authserver-basics/auth-server/src/main/java/com/example/config/SecurityFilterChainConfig.java
+++ authserver-basics/auth-server/src/main/java/com/example/config/SecurityFilterChainConfig.java
@@ -116,8 +116,7 @@ public class SecurityFilterChainConfig {
         // a JWT with information about the user. The /userinfo end requires the caller to provide
         // an access token, so the auth server reuses the spring security resource server support
         // to enforce security on the /userinfo end point
-
-      //  http.oauth2ResourceServer().jwt();
+        http.oauth2ResourceServer().jwt();
 
         // allows client apps to authenticate with the auth server using http basic authentication via client-id and
         // client-secret
